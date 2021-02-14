d501660ef7be41eb8e3693e062008e1fac3ad2a5
M1R41 N1KK1 <66789407+M1R4I-N1KK1@users.noreply.github.com>
2021-02-13 21:37:45 -0300

    v1.0

---
+++ brightness/brilho_down.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+
+def value():
+    return os.popen("xrandr --verbose --current | grep VGA1 -A5 | tail -n1").read()[13:]
+
+
+if '0.40' < value():
+    subprocess.check_output(["xrandr", "--output", "VGA1", "--brightness", str(float(value()) - 0.10)])
