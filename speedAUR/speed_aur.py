2cb828689e1d6e12678e6f358031a9f9653b7012
M1R41 N1KK1 <66789407+M1R4I-N1KK1@users.noreply.github.com>
2021-02-16 14:35:16 -0300

    v1.2 beta

---
+++ speedAUR/speed_aur.py
@@ -0,0 +1,26 @@
+import json
+import window
+from os import path
+
+directory = path.dirname(__file__)
+dl = 'curl'
+
+with open(directory + '/manager.json') as dm:
+    manager = json.load(dm)
+
+with open(directory + '/base', 'r') as file:
+    f = file.read()
+
+window.window_manager()
+option = int(input('choose an option: '))
+if option == 0:
+    dl = 'curl'
+if option == 1:
+    dl = 'axel'
+if option == 2:
+    dl = 'arie2'
+
+new_make = f.replace('MANAGER', str(manager[dl][0]))
+
+with open(directory + '/new_make.conf', 'w+') as make:
+    make.write(new_make)
---
+++ speedAUR/window.py
@@ -0,0 +1,18 @@
+def window_manager():
+    print("""
+*========================================*
+*      CHOOSE THE DONWLOAD MANAGER
+* (0) CURL ( default )
+* (1) AXEL
+* (2) ARIE2
+*========================================*""")
+
+
+def window_core():
+    print("""
+*========================================*
+*
+*   DEFINA O NÚMERO DE CORE'S DA SUA CPU
+*
+*
+*========================================*""")
