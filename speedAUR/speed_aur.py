bfcd3dfa975ae0f7dc9264d85a13a6cdf1d3b031
M1R41 N1KK1 <66789407+M1R4I-N1KK1@users.noreply.github.com>
2021-02-20 10:24:51 -0300

    v1.4 dev

--- speedAUR/speed_aur.py
+++ speedAUR/speed_aur.py
@@ -2,9 +2,13 @@ import json
 import window
 from os import path, system
 
+
+def clear():
+    system('clear')
+
+
 directory = path.dirname(__file__)
 dl = ['curl', 'axel', 'aria2']
-core = 2
 
 with open(directory + '/manager.json') as dm:
     manager = json.load(dm)
@@ -13,17 +17,21 @@ with open(directory + '/base', 'r') as file:
     f = file.read()
 
 while True:
-    system('clear')
     window.window_manager()
-    option = int(input('choose an option: '))
-    if option == 0 or option == 1 or option == 2:
-        dl = dl[option]
+    option = input('choose an option: ')
+    if option == '0' or option == '1' or option == '2':
+        dl = dl[int(option)]
         break
 
-window.window_core()
-option_core = int(input('choose an option: '))
+while True:
+    window.window_core()
+    option_core = int(input('choose an option: '))
+    if option_core == 0:
+        option_core = 2
+        break
+    break
 new_make = f.replace('MANAGER', str(manager[dl][0]))
-new_make_core = new_make.replace('CORE', str(option_core))
+new_make_core = new_make.replace('CORE', str(option_core + 1))
 
 with open(directory + '/new_make.conf', 'w+') as make:
     make.write(new_make_core)
