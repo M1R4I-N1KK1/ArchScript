b8ac4ac6e338aa3b7760fc715a6571ff2d691924
M1R41 N1KK1 <66789407+M1R4I-N1KK1@users.noreply.github.com>
2021-02-20 16:22:07 -0300

    v1.6 dev

--- speedAUR/speed_aur.py
+++ speedAUR/speed_aur.py
@@ -3,35 +3,41 @@ import window
 from os import path, system
 
 
+def resource_path(relative_path):
+    return path.realpath(relative_path)
+
+
 def clear():
     system('clear')
 
 
-directory = path.dirname(__file__)
-dl = ['curl', 'axel', 'aria2']
+try:
+    dl = ['curl', 'axel', 'aria2']
+    base_json = str()
+    with open(resource_path('manager.json')) as download_manager:
+        manager = json.load(download_manager)
 
-with open(directory + '/manager.json') as dm:
-    manager = json.load(dm)
+    with open(resource_path('base.conf'), 'r') as base:
+        base_make = base.read()
 
-with open(directory + '/base', 'r') as file:
-    f = file.read()
+    while True:
+        window.window_manager()
+        option = input('choose an option: ')
+        if option == '0' or option == '1' or option == '2':
+            dl = dl[int(option)]
+            break
 
-while True:
-    window.window_manager()
-    option = input('choose an option: ')
-    if option == '0' or option == '1' or option == '2':
-        dl = dl[int(option)]
+    while True:
+        window.window_core()
+        option_core = int(input('choose an option: '))
+        if option_core == 0:
+            option_core = 2
+            break
         break
+    new_make = base_make.replace('MANAGER', str(manager[dl][0]))
+    new_make_core = new_make.replace('CORE', str(option_core + 1))
 
-while True:
-    window.window_core()
-    option_core = int(input('choose an option: '))
-    if option_core == 0:
-        option_core = 2
-        break
-    break
-new_make = f.replace('MANAGER', str(manager[dl][0]))
-new_make_core = new_make.replace('CORE', str(option_core + 1))
-
-with open(directory + '/new_make.conf', 'w+') as make:
-    make.write(new_make_core)
+    with open(resource_path('new_make_core.conf'), 'w+') as make:
+        make.write(new_make_core)
+except KeyboardInterrupt:
+    print('Goodbye!')
