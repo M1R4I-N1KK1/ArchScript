c0db9e5e05e6ef0361e609c8c478de36647f3232
M1R41 N1KK1 <66789407+M1R4I-N1KK1@users.noreply.github.com>
2021-02-20 08:38:35 -0300

    v1.3 dev

--- speedAUR/speed_aur.py
+++ speedAUR/speed_aur.py
@@ -1,9 +1,10 @@
 import json
 import window
-from os import path
+from os import path, system
 
 directory = path.dirname(__file__)
-dl = 'curl'
+dl = ['curl', 'axel', 'aria2']
+core = 2
 
 with open(directory + '/manager.json') as dm:
     manager = json.load(dm)
@@ -11,16 +12,18 @@ with open(directory + '/manager.json') as dm:
 with open(directory + '/base', 'r') as file:
     f = file.read()
 
-window.window_manager()
-option = int(input('choose an option: '))
-if option == 0:
-    dl = 'curl'
-if option == 1:
-    dl = 'axel'
-if option == 2:
-    dl = 'aria2'
+while True:
+    system('clear')
+    window.window_manager()
+    option = int(input('choose an option: '))
+    if option == 0 or option == 1 or option == 2:
+        dl = dl[option]
+        break
 
+window.window_core()
+option_core = int(input('choose an option: '))
 new_make = f.replace('MANAGER', str(manager[dl][0]))
+new_make_core = new_make.replace('CORE', str(option_core))
 
 with open(directory + '/new_make.conf', 'w+') as make:
-    make.write(new_make)
+    make.write(new_make_core)
